9d307871b7e6a153b17bd9efc7e6f742d9a5b6b6
victorpinon <victorpinonrattia@hotmail.com>
2018-09-21 11:51:42 +0200

    Ya funciona el sort; falta solo ordenar un poco los mensajes

--- src/Figura.java
+++ src/Figura.java
@@ -1,19 +1,17 @@
-public class Figura {
+abstract  public class Figura implements Comparable<Figura>{
     public double area;
-    public static double suma;
+    static public double suma;
 
-    public double getArea() {
-        return this.area;
-    }
-
-
-
-    public Figura(){
-
-    }
-
-    public Figura(double area){
+    public Figura(double area) {
         this.area = area;
         suma = suma + area;
     }
+
+    public int compareTo (Figura f) {
+        if (this.area==f.area)
+            return 0;
+        else
+            return -1;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(2,3);
         Rectangle r2 = new Rectangle(3,3);
         Cercle c1 = new Cercle(5);
-        Figura[] f = {c1,r1,r2};
-        System.out.println("La lista de arrays desordenada es:" +Arrays.toString(f));
-        System.out.println("La suma de todas las areas es:" +Figura.suma);
+
+        Figura[] f = new Figura[3];
+        f[0] = r1;
+        f[1] = r2;
+        f[2] = c1;
+
+        System.out.println("Lista desordenada");
+        for (Figura i : f) {
+            System.out.println(i);
+        }
+
+        System.out.println("Lista ordenada");
+        Arrays.sort(f);
+        for (Figura i : f) {
+            System.out.println(i);
+        }
+
+        System.out.println("La suma es de todas las figuras es:" + Figura.suma);
     }
-
-
-
-
 }
