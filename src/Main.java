dc0e48315bd4c86d477c5470c6e2f793c80be75b
victorpinon <victorpinonrattia@hotmail.com>
2018-09-21 10:35:16 +0200

    He puesto un display de las figuras

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle1 = new Rectangle(2,3);
-        Rectangle rectangle2 = new Rectangle(3,3);
-        Cercle cercle1 = new Cercle(5);
-        System.out.println(Figura.suma);
+        Rectangle r1 = new Rectangle(2,3);
+        Rectangle r2 = new Rectangle(3,3);
+        Cercle c1 = new Cercle(5);
+        Figura[] f = {c1,r1,r2};
+        System.out.println("La lista de arrays desordenada es:" +Arrays.toString(f));
+        System.out.println("La suma de todas las areas es:" +Figura.suma);
     }
+
+
 
 
 }
